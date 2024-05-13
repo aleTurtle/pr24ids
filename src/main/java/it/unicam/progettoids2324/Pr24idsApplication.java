482f8641340cd46c9ea016bf294b4866316274f8
turtle <alessio.tartufoli@studenti.unicam.it>
2024-05-13 12:19:53 +0200

    Initial commit

---
+++ src/main/java/it/unicam/progettoids2324/Pr24idsApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.progettoids2324;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pr24idsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pr24idsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/unicam/progettoids2324/Pr24idsApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unicam.progettoids2324;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pr24idsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
